f05caf6203315b109dbdca763cc0070304a29d22
leonardo.lopes@golivetech.com.br <leonardo.lopes@golivetech.com.br>
2024-11-23 13:56:26 -0300

    upload to vercell

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+function Home() {
+    return (
+        <div>
+            <h1>Te amo, princesinha S2</h1>
+            <Image src="/img/kiss-blushing.gif" alt="Beijo com blush" width={300} height={300} />
+        </div>
+    );
+}
+
+export default Home;
