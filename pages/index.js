179b33810ddf7b50d5daed3eb87be615843dffe9
leonardo.lopes@golivetech.com.br <leonardo.lopes@golivetech.com.br>
2024-11-24 19:56:20 -0300

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Image from "next/image";
 
 function Home() {
-    return (
-        <div>
-            <h1>Te amo, princesinha S2</h1>
-            <Image src="/img/kiss-blushing.gif" alt="Beijo com blush" width={300} height={300} />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Te amo, princesinha S2</h1>
+      <Image
+        src="/img/kiss-blushing.gif"
+        alt="Beijo com blush"
+        width={300}
+        height={300}
+      />
+    </div>
+  );
 }
+function teste2() {
+  asdasdasd;
 
+  AudioParamMap;
+}
 export default Home;
