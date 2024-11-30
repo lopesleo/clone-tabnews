b66bea42c781b11cf2a177c7e75d56bbcb59473d
lopesleo <leonardo.lopes-almeida@hotmail.com>
2024-11-30 20:44:21 -0300

    adicionando  , e

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import { RESPONSE_LIMIT_DEFAULT } from "next/dist/server/api-utils/index.js";
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ status: "Bora apostar!" });
 }
 
